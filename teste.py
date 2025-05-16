be08b41f59b01ed0eee01a2ec0f0a84964069ed2
Gabriel <gabriel66585@gmail.com>
2025-05-16 15:14:11 -0300

    criação do main
    Signed-off-by: Gabriel <gabriel66585@gmail.com>

---
+++ teste.py
@@ -0,0 +1,12 @@
+import numpy   
+import time
+from math import sqrt
+A = int(input("Digite um numero para saber sua raiz: "))
+
+h = sqrt(A)
+print(f"A raiz de {A} é {h}")
+
+a = numpy.zeros(10000)
+print(f'conteudo {a}')
+
+start_time = time.time()
